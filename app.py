5ff520fbf9bf780bf1c09fcf9ff277a0b31717a4
TraderFs69 <fsanscartier@hotmail.com>
2026-06-10 18:50:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 from streamlit_sortables import sort_items
-
+from streamlit_local_storage import LocalStorage
+st.write("Local Storage chargé")
 # ==================================================
 # CONFIG
 # ==================================================
